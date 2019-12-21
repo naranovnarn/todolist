a3a988ab5ffe0e52577c2b7230d389b71f7b969b
naranovnarn <na.naranov@gmail.com>
2019-12-21 17:31:22 +0400

    added deleted task

--- javascript.js
+++ javascript.js
@@ -9,56 +9,69 @@ document.addEventListener('DOMContentLoaded', function () {
         const completeButton = document.querySelector('.action__completed');
         const activeButton = document.querySelector('.action__active');
         const allTasks = document.querySelector('.action__alltasks');
+        let id = 0;
 
 
         addButton.addEventListener('click', addTask);
+        input.addEventListener('keydown', addEnter);
+        // completeButton.addEventListener('click', CompletedTasks)
 
         function addTask() {
-
             const taskName = input.value;
 
-            todoList.add(taskName);
+            todoList.add(taskName, id);
+            
+            id++;
 
-            render();
+            render(todoList.getAll());
         }
 
-        input.addEventListener('keydown', addEnter);
 
         function addEnter(e) {
-            if (event.code == 'Enter') {
+            if (e.code == 'Enter') {
                 addTask();
             }
         }
 
-
-        function render() {
-
+        function render(tasks) {
             removeTasks();
 
-            const newArr = todoList.getAll();
-
-            newArr.forEach((item, index) => {
+            tasks.forEach((item) => {
                 const li = document.createElement('li');
+
                 li.classList.add('task-list__item');
-                li.setAttribute('data-index', index);
-
+                li.setAttribute('data-index', item.id);
                 li.innerHTML = `<input type="checkbox" ${item.comleted ? "checked" : ''}> ${item.name} <i class="fa fa-trash-o delete-task"></i> `;
-                const iconDelete = li.lastElementChild;
-                iconDelete.addEventListener('click', deleteTask);
 
+                const iconDelete = li.lastElementChild;
                 const copmleteCheckbox = li.firstElementChild;
-                copmleteCheckbox.addEventListener('change', completeTask);
+
+                iconDelete.addEventListener('click', deleteTask);
+                copmleteCheckbox.addEventListener('change', changeStatusTask);
 
                 taskList.appendChild(li);
             });
+
+            resetInputValue();
+        }
+
+        function renderCompletedTasks() {
+            render(todoList.getCompleted());
+        }
+
+        function renderActive() {
+            render(todoList.getActive());
         }
 
         function deleteTask(event) {
-            const index = event.target.parentElement.getAttribute('data-index');
+            const id = +event.target.parentElement.getAttribute('data-index');
+            todoList.remove(id);
+            render(todoList.getAll());
         }
 
-
-        
+        function resetInputValue() {
+            input.value = '';
+        }
 
         function removeTasks()  {
             while (taskList.firstChild) {
@@ -66,28 +79,28 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
 
-        function completeTask() {
-            if (this.checked) {
-                console.log(this.parentElement.innerText);
+        function changeStatusTask(event) {
+            if (event.target.checked) {
+                // Найти по id таску, и поменять у нее свойство completed с false на true и наоборот.
+                const id = +event.target.parentElement.getAttribute('data-index');
+                console.log(id)
                 return this.parentElement.classList.add('_completed');
             }
+
             this.parentElement.classList.remove('_completed');
         }
     }
 
 );
 
-
-
-
-
 function TodoList() {
     this.list = [];
-
-    this.add = function (name) {
+    
+    this.add = function (name, id) {
         this.list.push({
             name: name,
             completed: false,
+            id: id
         });
     }
 
@@ -108,10 +121,9 @@ function TodoList() {
     }
 
     this.remove = function (id) {
-        const indexDelete = this.list.findIndex(item => item.id);
-        this.list.splice(index, 1);
+        const indexDelete = this.list.findIndex(item => item.id == id);
+        this.list.splice(indexDelete, 1);
     }
-
 }
 
 
